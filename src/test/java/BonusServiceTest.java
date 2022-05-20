152a6bc55a87e5134dba0ba522e8c76021629b38
Ayrton <devayrtonsbarreto@gmail.com>
2022-05-19 23:57:04 -0300

    Criando a classe BonusServiceTest e testando três situações que podem ocorrer

---
+++ src/test/java/BonusServiceTest.java
@@ -0,0 +1,35 @@
+import br.com.alura.tdd.modelo.Funcionario;
+import br.com.alura.tdd.service.BonusService;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.junit.Assert.assertEquals;
+
+public class BonusServiceTest {
+
+    @Test
+    public void bonusDeveriaSerZeroParaFuncionarioComSalarioMuitoAlto() {
+        Funcionario funcionario = new Funcionario("Ayrton", LocalDate.now(), new BigDecimal("25000"));
+        BonusService service = new BonusService();
+        BigDecimal bonus = service.calcularBonus(funcionario);
+        assertEquals(BigDecimal.ZERO, bonus);
+    }
+
+    @Test
+    public void bonusDeveriaSer10PorCentoDoSalario() {
+        Funcionario funcionario = new Funcionario("Ayrton", LocalDate.now(), new BigDecimal("2500"));
+        BonusService service = new BonusService();
+        BigDecimal bonus = service.calcularBonus(funcionario);
+        assertEquals(new BigDecimal("250.0"), bonus);
+    }
+
+    @Test
+    public void bonusDeveriaSerDezPorCentoParaSalarioDeExatamente10000() {
+        Funcionario funcionario = new Funcionario("Ayrton", LocalDate.now(), new BigDecimal("10000"));
+        BonusService service = new BonusService();
+        BigDecimal bonus = service.calcularBonus(funcionario);
+        assertEquals(new BigDecimal("1000.0"), bonus);
+    }
+}
